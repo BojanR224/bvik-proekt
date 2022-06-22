0bf04eb76cce7d07681a32c17018a45fa1aac054
BojanRobev <bojan.robev@gmail.com>
2022-06-22 13:00:48 +0200

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bvik-proekt
  
+ # Setting up env
+ pip install uvicorn
+ pip install fastapi
+ pip install aleph_client
+
+ # starting the application
+ 
+bojan@Bojan:/bvikproekt/backend$ uvicorn --reload --host=0.0.0.0 --port=8080 app.api:app
--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
 
 # conda activate bvik-env
-# Ovaa komanda pisi ja vo wsl terminal uvicorn --reaload --host=0.0.0.0 --port=8080 app.:app
+# Ovaa komanda pisi ja vo wsl terminal uvicorn --reaload --host=0.0.0.0 --port=8080 app.api:app
