423e550ceb30967a94f72d1de39b5ead963be0c3
BojanRobev <bojan.robev@gmail.com>
2022-07-06 21:26:21 +0200

    add aggregate

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,17 +1,20 @@
 import string
+from coincurve import PrivateKey
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from aleph_client.synchronous import create_aggregate, get_messages
+from aleph_client.chains.ethereum import ETHAccount
 
 app = FastAPI()
+
+private_key = bytes.fromhex("f12ee701be6699b7e597b1ce7a76b7cc82b0be6abb27007091403d3a50b72f01")
+
+account = ETHAccount(private_key)
 
 origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
-
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,7 +27,8 @@ app.add_middleware(
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
-    return {"message": "Welcome to your todo list."}
+    aggregate_message = await create_aggregate(account, 'testKey',{'1':1}, channel='BViK-proekt')
+    return {"message": aggregate_message}
 
 # @app.get("/v1/{id}")
 # async def list_all_messages(id: str):
