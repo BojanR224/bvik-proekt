6b50f550ddfc3e25a0b78ebeba6167343053413f
BojanRobev <bojan.robev@gmail.com>
2022-07-08 12:11:45 +0200

    create pydantic model

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,11 +1,10 @@
-import string
 from webbrowser import get
-from coincurve import PrivateKey
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from aleph_client.synchronous import create_aggregate, get_messages, fetch_aggregate
 from aleph_client.chains.ethereum import ETHAccount
 import json
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,38 +12,51 @@ private_key = bytes.fromhex("d2c56ff4e0dc6d23185f5fe6d85c96af8512a8fce3cf576f893
 
 account = ETHAccount(private_key)
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
+# origins = [
+#     "http://localhost:3000",
+#     "localhost:3000"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
+class NFT(BaseModel):
+    name: str
+    description: str 
+    link: str
 
 
 @app.get("/", tags=["root"])
 async def root() -> dict:
     return {"message": "Hello world"}
 
-# get : da zima spesificno nft od cel json
-@app.get("/get")
-async def get_nft() -> str:
-    #nft_json = fetch_aggregate(account.get_address(), key='bvik-proekt')
+# contract e klucot
+# id e adresata na nft
 
-    aggregate_message = create_aggregate(account, key='key', content={'id': 23}, address='0xeB1ebA7a4fa4F05e369035c7f97C0f046F550C28')
-    json_message = json.dumps(aggregate_message)
-    #print(json_message['item_content']['content']['id'])
-    return None
+@app.get("/{key}")
+async def get_nft(contract: str, id: str) -> dict:
+    nft_json = fetch_aggregate(account.get_address(), key=contract)
+    nft_json = json.dumps(nft_json)
+    
+    return nft_json[id]
 
 # put : dodavame nft vo kolekcijata, prva treba da se proveri dali go ima vo kolekicata; ako go ima se menja se "pravi neso"; ako go nema go cel nft vo kolekcija
-@app.get("/put")
-async def put_nft() -> dict:
-    return None
+
+@app.get("/{contract}/{id}")
+async def put_nft(contract: str, id: str, json: str) -> dict:
+    nft_json = fetch_aggregate(account.get_address(), key=contract)
+    nft_json = json.dumps(nft_json)
+
+    #aggregate_message = create_aggregate(account, key='key', content={'id': 23}, address='0xeB1ebA7a4fa4F05e369035c7f97C0f046F550C28')
+    #json_message = json.dumps(aggregate_message)
+    #print(json_message['item_content']['content']['id'])
+
+    return nft_json
+
 
 # vo urlto network/id
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
-import uvicorn
+import hypercorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    hypercorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
 
 # conda activate bvik-env
 # Ovaa komanda pisi ja vo wsl terminal uvicorn --reload --host=0.0.0.0 --port=8080 app.api:app
